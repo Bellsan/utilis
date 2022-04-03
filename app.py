a356d31606f2a1e23d91aa81586a0ffc0f859fc3
Elliot Steffensen <elliot@steffensens.se>
2022-04-03 21:01:32 +0200

    Implemented file upload and download

--- app.py
+++ app.py
@@ -5,8 +5,9 @@ Documentation
 See also https://www.python-boilerplate.com/flask
 """
 import os
+import datetime
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 from flask_cors import CORS
 
 
@@ -24,12 +25,18 @@ def create_app(config=None):
     # Definition of the routes. Put them into their own file. See also
     # Flask Blueprints: http://flask.pocoo.org/docs/latest/blueprints
     @app.route("/")
-    def hello_world():
-        return "Hello World"
+    def home():
+        return render_template('home.html', utc=datetime.datetime.utcnow())
 
-    @app.route("/foo/<id>")
-    def foo_url_arg(id):
-        return jsonify({"echo": id})
+    @app.route("/excel", methods=('GET', 'POST'))
+    def excel():
+        directory = "files"
+        files = []
+        for filename in os.listdir(directory):
+            f = os.path.join(directory, filename)
+            if os.path.isfile(f):
+                files.append(f)
+        return render_template('excel.html', files=files)
 
     return app
 
